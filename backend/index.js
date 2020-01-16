ff23e02f58c82af8b741c9a9f9e01fbbdcd9b409
wandersonSantiago <wandersonsantiago86@gmail.com>
2020-01-16 03:50:43 -0300

    add authentication a JWT

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,17 @@
 const app = require('express')()
 const consign = require('consign')
 const db = require('./config/db')
+const mongoose = require('mongoose')
+
+require('./config/mongodb')
+
+app.db = db
+app.mongoose = mongoose
 
 app.db = db
 
 consign()
+    .include('./config/passport.js')
     .then('./config/middlewares.js')
     .then('./api/validation.js')
     .then('./api')
